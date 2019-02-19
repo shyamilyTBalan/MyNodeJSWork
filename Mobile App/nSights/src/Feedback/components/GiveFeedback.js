f5cfadfac4c8bbe839569e928b0837af49045edb
LiyaJohn <liya@systalent.com>
2019-02-19 17:38:59 +0530

    leaderboards changes

--- Mobile App/nSights/src/Feedback/components/GiveFeedback.js
+++ Mobile App/nSights/src/Feedback/components/GiveFeedback.js
@@ -144,14 +144,14 @@ export default class GiveFeedback extends Component {
             <List >
               {filteredEmails.map((data, i) => (
                 <ListItem avatar noBorder>
-                  <TouchableOpacity onPress={() => this.props.navigate('Feedback')}>
+                  <TouchableOpacity >
                     <Left>
                       <Thumbnail source={data.img} />
                     </Left>
                   </TouchableOpacity>
 
                   <Body>
-                    <TouchableOpacity onPress={() => this.props.navigate('Feedback')}>
+                    <TouchableOpacity >
                       <Text style={styles.textname}>{data.name}</Text>
                       <Text style={styles.textnote} numberOfLines={1} note>
                         {data.note}
